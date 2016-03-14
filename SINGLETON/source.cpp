01a9b1cdbbecd3f89492d5b091160874cc06b02e
Bartłomiej Romanek <rombarte@users.noreply.github.com>
2016-03-14 23:09:08 +0100

    Update source.cpp

--- SINGLETON/source.cpp
+++ SINGLETON/source.cpp
@@ -1,3 +1,8 @@
+/* Autor: Bartłomiej Romanek          */
+/* Tytuł: Wzorzec Singleton           */
+/* Licencja: Licencja MIT             */
+/* Data modyfikacji: 14.03.2016       */
+
 #include <iostream>
 
 class Singleton {
@@ -24,3 +29,6 @@ int main(){
 	Singleton::Wykorzystaj().Zarejestruj();
 	Singleton::Wykorzystaj();
 }
+
+// Opis kodu: Klasa Singleton pozwala na utworzenie wyłącznie jednego egzemplarza klasy w trakcie działania programu.
+// Obiekt ten jest tworzony przy pierwszej próbie użycia.
